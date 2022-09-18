9b5b9ac04ac95a2128510faff1a9ccb7696978f4
Robert Bykowski <trebor.media@gmail.com>
2022-09-18 17:13:02 +0200

    This is a new code template.

---
+++ templatestr.py
@@ -0,0 +1,25 @@
+
+# name = input("Write your name: ")
+# family_name = input("Write your family name: ")
+# print("Twoje imie to"+name)
+# print("Twoje nazwisko to "+family_name)
+
+# List - how to creat list
+#friends = ["Kevin", "Karen", "Tom"]
+#              0       1        3   Negative value -1
+#print(friends[0])
+#print(friends[1:2])
+# Change one element of this list
+#friends[1] = "Mike"
+#print(friends)
+
+# List funkcions
+lucky_numbers = [4, 8, 9, 10, 56]
+friends = ["Kevin", "Karen", "Tom"]
+#friends.extend(lucky_numbers) #Extend your list on this way
+friends.append("Julia") # Add one element to list
+print(friends)
+
+
+
+
