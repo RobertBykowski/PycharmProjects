8909de04c37d6523975a1f6b8ce6fd694059483f
Robert Bykowski <trebor.media@gmail.com>
2022-09-20 20:12:23 +0200

    Add into files and other options.

--- loop_tests.py
+++ loop_tests.py
@@ -1,14 +1,15 @@
-import random
-
 tablica1 = []
 tablica2 = ("X", "*", ".")
-
-for a in tablica2:
-     tablica1.append(a)
-     print(tablica1, end ="")
-     fields_file = open("fields.txt", "a")
-     fields_file.readable()
-     #znak_bis = map(lambda x: x + "\n", tablica1)
-     #fields_file.writelines(znak_bis)
-     fields_file.write(str(tablica1))
-     fields_file.close()
+n = 0
+a = ""
+while(n<1): #warunek
+    n=n+1
+    for a in tablica2:
+        tablica1.append(a)
+        print(tablica1, end="")
+        fields_file = open("fields.txt", "a")
+        fields_file.readable()
+        # znak_bis = map(lambda x: x + "\n", tablica1)
+        # fields_file.writelines(znak_bis)
+        fields_file.write(str(tablica1))
+        fields_file.close()
--- loop_while.py
+++ loop_while.py
@@ -1,15 +1,22 @@
-tablica1 = []
-tablica2 = ("X", "*", ".")
-n = 0
-a = ""
-while(n<2): #warunek
-    n=n+1
-    for a in tablica2:
-        tablica1.append(a)
-        print(tablica1, end="")
-        fields_file = open("fields.txt", "a")
-        fields_file.readable()
-        # znak_bis = map(lambda x: x + "\n", tablica1)
-        # fields_file.writelines(znak_bis)
-        fields_file.write(str(tablica1))
-        fields_file.close()
+import random
+tab1 = ["X", "*", ".",]
+tab2 = []
+a=0
+while a < 30: #len(tab1)
+    fields_file = open("fields.txt", "a")
+    fields_file.readable()
+    x = random.choices(tab1, k=5)
+    print(x)
+    '''tab2.append(x)
+    print(tab2, "\n")'''
+    fields_file.write(str(x))
+    fields_file.close()
+    '''print(x, end = "")
+    print("\n")
+    print(x, end="")
+    print("\n")'''
+    a += 1
+
+
+
+
