c3ae46a153b9b611b46cdd8409df6f4e993d6daa
Robert Bykowski <trebor.media@gmail.com>
2022-09-22 13:49:25 +0200

    Wersja z zapisaem danych w pliku

--- Files_reading.py
+++ Files_reading.py
@@ -5,13 +5,21 @@ def znajdz_wzorzec(text, wzorzec):
     liczba_wystapien = 0
     dl1 = len(text)
     dl2 = len(wzorzec)
+    tablica_pozycji_wysapien = []
 
 
     for i in range (dl1-dl2+1):
         if text [i] == wzorzec[0]:
+            tablica_pozycji_wysapien.clear()
             if text [i:i+dl2] == wzorzec:
-                print(f"Znaleziono wzorzec {wzorzec} na pozycji {i}")
+                tablica_pozycji_wysapien.append(i)
+                #print(tablica_pozycji_wysapien)
+                with open("tablica_pozycji_wysapien.txt", 'a') as file:
+                    file.write(str(tablica_pozycji_wysapien))
+                #print(f"Znaleziono wzorzec {wzorzec} na pozycji {i}")
+
                 liczba_wystapien += 1
+
     if liczba_wystapien == 0:
         print(f"Nie znaleziono wzoraca {wzorzec}")
     else:
@@ -19,8 +27,10 @@ def znajdz_wzorzec(text, wzorzec):
 
 with open("email_ANSI.txt", mode = 'r', encoding='cp1250') as f:
     tekst = f.read()
-    print(tekst)
+    #print(tekst[17643-2])
+
 znajdz_wzorzec(tekst, ".pl")
+
 
 
 
