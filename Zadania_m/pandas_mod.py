afc87d2041abe2416130cbe4b0c9b8811c49620c
Robert Bykowski <trebor.media@gmail.com>
2022-10-23 20:00:24 +0200

    The next week of coding.

---
+++ Zadania_m/pandas_mod.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# a = [['Ania',24, 3400],['Michał',9, 5400],['Darek',40, 6000],['Ewa',43]]
+# df_a = pd.DataFrame(a)
+# df_a.columns = 'Imię', 'Wiek', 'Wypłata'
+# print(df_a[0:2])
+list = ["Ola", "Ala", "Toma", "Ron"]
+tab = pd.DataFrame(list)
+tab.columns = "Imię",
+print(tab)
+
