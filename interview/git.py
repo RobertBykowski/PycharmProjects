02aa049e1af9649fe41101aa9f68ad346d8ef305
Robert Bykowski <trebor.media@gmail.com>
2022-12-19 18:04:08 +0100

    {commit_mesg}

--- interview/git.py
+++ interview/git.py
@@ -5,9 +5,9 @@ os.chdir('C:\\Users\\Admin\\PycharmProjects\\pythonProject1')
 
 # Wykonuje comende add
 os.system('git add -A')
-
+commit_mesg = input("Podaj komentarz dla commita: ")
 # Wykonuje commit z podaną wiadomościąpyinstaller
-os.system('git commit -am "Dodano nowy plik"')
+os.system('git commit -am "{commit_mesg}"')
 
 # Wykonuje push do repozytorium
 os.system('git push')
